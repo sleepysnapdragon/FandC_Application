8d69125eeee9810f3b1bdb85b34e71980bcbf37b
Gillian <gillian@yeomans.org.uk>
2019-08-21 18:12:08 +0200

    changed colours, added 'why' structure, other layout edits

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,11 @@
+var canvasWidth = document.getElementById("canvas").clientWidth;
+var canvasHeight = canvasWidth * 0.75;
+//need code to check if it resizes
+
 var context;
 var currentSlide = 0;
 x1 = 0;
-x2 = 800;
+x2 = 640;
 xt = 3;
 var trns;
 var repeat;
@@ -19,7 +23,7 @@ function auto() {
   //begin display when first image is ready
   pics[0].onload = function() {
     context = canvas.getContext("2d");
-    context.drawImage(pics[currentSlide], x1, 0, 800, 600);
+    context.drawImage(pics[currentSlide], x1, 0, canvas.width, 480);
     repeat = setTimeout(function() {
       startTrans(pics);
     }, 3000);
@@ -33,16 +37,16 @@ function startTrans(pics) {
 }
 
 function changePic(pics) {
-  context.clearRect(0, 0, 800, 600);
+  context.clearRect(0, 0, 640, 480);
   var nextSlide = currentSlide == pics.length - 1 ? 0 : currentSlide + 1;
-  context.drawImage(pics[currentSlide], x1, 0, 800, 600);
-  context.drawImage(pics[nextSlide], x2, 0, 800, 600);
-  if (x1 > -800) {
+  context.drawImage(pics[currentSlide], x1, 0, 640, 480);
+  context.drawImage(pics[nextSlide], x2, 0, 640, 480);
+  if (x1 > -640) {
     x1 -= xt;
     x2 -= xt;
   } else {
     x1 = 0;
-    x2 = 800;
+    x2 = 640;
     if (currentSlide + 1 > pics.length - 1) {
       currentSlide = 0;
       clearInterval(trns);
@@ -61,29 +65,31 @@ function stopAuto() {
 
 function showX(x) {
   stopAuto();
-  context.clearRect(0, 0, 800, 600);
+  context.clearRect(0, 0, 640, 480);
   currentSlide = x;
-  context.drawImage(pics[currentSlide], 0, 0, 800, 600);
+  context.drawImage(pics[currentSlide], 0, 0, 640, 480);
 }
 
 function next() {
   stopAuto();
-  context.clearRect(0, 0, 800, 600);
+  context.clearRect(0, 0, 640, 480);
   if (currentSlide == 2) {
     currentSlide = 0;
   } else {
     currentSlide++;
   }
-  context.drawImage(pics[currentSlide], 0, 0, 800, 600);
+  context.drawImage(pics[currentSlide], 0, 0, 640, 480);
 }
 
 function previous() {
   stopAuto();
-  context.clearRect(0, 0, 800, 600);
+  context.clearRect(0, 0, 640, 480);
   if (currentSlide == 0) {
     currentSlide = 2;
   } else {
     currentSlide--;
   }
-  context.drawImage(pics[currentSlide], 0, 0, 800, 600);
+  context.drawImage(pics[currentSlide], 0, 0, 640, 480);
 }
+
+window.onload = auto();
